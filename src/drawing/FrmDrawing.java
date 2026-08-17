56693539328df10297f9ed2798040b9c6beb2567
MilicaIT7 <grujic.it7.2024@uns.ac.rs>
2026-08-17 22:08:03 +0200

    Kreiran osnovni dizajn Drawing frame-a i panel-a

---
+++ src/drawing/FrmDrawing.java
@@ -0,0 +1,153 @@
+package drawing;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+import javax.swing.border.EmptyBorder;
+
+public class FrmDrawing extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JPanel pnlDrawingArea;
+	private JPanel pnlActions;
+	private JPanel pnlTitle;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmDrawing frame = new FrmDrawing();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmDrawing() {
+		setTitle("Grujić Milica, IT 7/2024");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 471, 350);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setBackground(new Color(132, 161, 196));
+
+		pnlTitle = new JPanel();
+		pnlTitle.setBackground(new Color(132, 161, 196));
+		contentPane.add(pnlTitle, BorderLayout.NORTH);
+
+		JLabel lblTitle = new JLabel("DRAWING APPLICATION");
+		lblTitle.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		pnlTitle.add(lblTitle);
+
+		pnlDrawingArea = new JPanel();
+		pnlDrawingArea.setBackground(new Color(232, 240, 250));
+		pnlDrawingArea.setLayout(new BorderLayout(0, 0));
+		contentPane.add(pnlDrawingArea, BorderLayout.CENTER);
+
+		JPanel pnlShapes = new JPanel();
+		pnlShapes.setBackground(new Color(232, 240, 250));
+		pnlShapes.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		pnlDrawingArea.add(pnlShapes, BorderLayout.NORTH);
+
+		JToggleButton tglbtnPoint = new JToggleButton("Point");
+		tglbtnPoint.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		tglbtnPoint.setBackground(new Color(132, 161, 196));
+		tglbtnPoint.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createLineBorder(new Color(95, 120, 150), 2),
+				BorderFactory.createEmptyBorder(4, 12, 4, 12)));
+		buttonGroup.add(tglbtnPoint);
+		pnlShapes.add(tglbtnPoint);
+
+		JToggleButton tglbtnLine = new JToggleButton("Line");
+		tglbtnLine.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		tglbtnLine.setBackground(new Color(132, 161, 196));
+		tglbtnLine.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createLineBorder(new Color(95, 120, 150), 2),
+				BorderFactory.createEmptyBorder(4, 12, 4, 12)));
+		buttonGroup.add(tglbtnLine);
+		pnlShapes.add(tglbtnLine);
+
+		JToggleButton tglbtnRectangle = new JToggleButton("Rectangle");
+		tglbtnRectangle.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		tglbtnRectangle.setBackground(new Color(132, 161, 196));
+		tglbtnRectangle.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createLineBorder(new Color(95, 120, 150), 2),
+				BorderFactory.createEmptyBorder(4, 12, 4, 12)));
+		buttonGroup.add(tglbtnRectangle);
+		pnlShapes.add(tglbtnRectangle);
+
+		JToggleButton tglbtnCircle = new JToggleButton("Circle");
+		tglbtnCircle.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		tglbtnCircle.setBackground(new Color(132, 161, 196));
+		tglbtnCircle.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createLineBorder(new Color(95, 120, 150), 2),
+				BorderFactory.createEmptyBorder(4, 12, 4, 12)));
+		buttonGroup.add(tglbtnCircle);
+		pnlShapes.add(tglbtnCircle);
+
+		JToggleButton tglbtnDonut = new JToggleButton("Donut");
+		tglbtnDonut.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		tglbtnDonut.setBackground(new Color(132, 161, 196));
+		tglbtnDonut.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createLineBorder(new Color(95, 120, 150), 2),
+				BorderFactory.createEmptyBorder(4, 12, 4, 12)));
+		buttonGroup.add(tglbtnDonut);
+		pnlShapes.add(tglbtnDonut);
+		
+		PnlDrawing pnlDrawing = new PnlDrawing();
+		pnlDrawingArea.add(pnlDrawing, BorderLayout.CENTER);
+
+		pnlActions = new JPanel();
+		pnlActions.setBackground(new Color(232, 240, 250));
+		pnlActions.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		contentPane.add(pnlActions, BorderLayout.SOUTH);
+
+		JButton btnSelect = new JButton("Select");
+		btnSelect.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		btnSelect.setBackground(new Color(132, 161, 196));
+		btnSelect.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createLineBorder(new Color(95, 120, 150), 2),
+				BorderFactory.createEmptyBorder(4, 12, 4, 12)));
+		pnlActions.add(btnSelect);
+
+		JButton btnModify = new JButton("Modify");
+		btnModify.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		btnModify.setBackground(new Color(132, 161, 196));
+		btnModify.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createLineBorder(new Color(95, 120, 150), 2),
+				BorderFactory.createEmptyBorder(4, 12, 4, 12)));
+		pnlActions.add(btnModify);
+
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		btnDelete.setBackground(new Color(132, 161, 196));
+		btnDelete.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createLineBorder(new Color(95, 120, 150), 2),
+				BorderFactory.createEmptyBorder(4, 12, 4, 12)));
+		pnlActions.add(btnDelete);
+
+	}
+
+}
---
+++ src/drawing/PnlDrawing.java
@@ -0,0 +1,18 @@
+package drawing;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class PnlDrawing extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public PnlDrawing() {
+		setBackground(new Color(245, 249, 253));
+	}
+
+}
