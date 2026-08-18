353a66e44506e77d9474b1713df869f86f403ccb
MilicaIT7 <grujic.it7.2024@uns.ac.rs>
2026-08-18 04:37:35 +0200

    Dodata lista oblika i metoda za iscrtavanje u PnlDrawing

--- src/drawing/PnlDrawing.java
+++ src/drawing/PnlDrawing.java
@@ -1,18 +1,38 @@
 package drawing;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
+
+import geometry.Shape;
 
 public class PnlDrawing extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private ArrayList<Shape> shapes = new ArrayList<Shape>();
 
 	/**
 	 * Create the panel.
 	 */
 	public PnlDrawing() {
 		setBackground(new Color(245, 249, 253));
+	}
+	
+	public void paint(Graphics g) {
+		super.paint(g);
+		for (Shape shape : shapes) {
+			shape.draw(g);
+		}
+	}
+
+	public ArrayList<Shape> getShapes() {
+		return shapes;
+	}
+
+	public void setShapes(ArrayList<Shape> shapes) {
+		this.shapes = shapes;
 	}
 
 }
