5167c1deccda6214f4fee830f0471be1543b0510
MilicaIT7 <grujic.it7.2024@uns.ac.rs>
2026-08-27 14:32:30 +0200

    Dodato brisanje donut-a

--- src/drawing/FrmDrawing.java
+++ src/drawing/FrmDrawing.java
@@ -514,6 +514,16 @@ public class FrmDrawing extends JFrame {
 						pnlDrawing.repaint();
 					}
 				}
+				
+				if (selectedShape instanceof Donut) {
+					int confirm = JOptionPane.showConfirmDialog(null,
+							"Are you sure you want to delete this donut?", "Confirm deletion",
+							JOptionPane.YES_NO_OPTION);
+					if (confirm == JOptionPane.YES_OPTION) {
+						pnlDrawing.getShapes().remove(selectedShape);
+						pnlDrawing.repaint();
+					}
+				}
 			}
 		});
 		btnDelete.setFont(new Font("Times New Roman", Font.BOLD, 12));
