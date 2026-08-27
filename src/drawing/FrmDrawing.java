0ea5252137c296c9448bb86bcf3af05f75996185
MilicaIT7 <grujic.it7.2024@uns.ac.rs>
2026-08-27 17:32:55 +0200

    Dodato brisanje kruga

--- src/drawing/FrmDrawing.java
+++ src/drawing/FrmDrawing.java
@@ -523,7 +523,15 @@ public class FrmDrawing extends JFrame {
 						pnlDrawing.getShapes().remove(selectedShape);
 						pnlDrawing.repaint();
 					}
-				}
+				} else if (selectedShape instanceof Circle) {
+					int confirm = JOptionPane.showConfirmDialog(null,
+							"Are you sure you want to delete this circle?", "Confirm deletion",
+							JOptionPane.YES_NO_OPTION);
+					if (confirm == JOptionPane.YES_OPTION) {
+						pnlDrawing.getShapes().remove(selectedShape);
+						pnlDrawing.repaint();
+					}
+				}		
 			}
 		});
 		btnDelete.setFont(new Font("Times New Roman", Font.BOLD, 12));
